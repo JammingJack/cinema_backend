e39bc149ec12a8385e9068e2323cf029373bef4a
JammingJack <amineseth1@gmail.com>
2021-05-26 20:00:21 +0100

    enabled cross origin access

--- src/main/java/com/enset/cineam_app/dao/ProjectionRepository.java
+++ src/main/java/com/enset/cineam_app/dao/ProjectionRepository.java
@@ -3,7 +3,9 @@ package com.enset.cineam_app.dao;
 import com.enset.cineam_app.entities.Projection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource
+@CrossOrigin("*")
 public interface ProjectionRepository extends JpaRepository<Projection,Long> {
 }
--- src/main/java/com/enset/cineam_app/dao/SeanceRepository.java
+++ src/main/java/com/enset/cineam_app/dao/SeanceRepository.java
@@ -2,6 +2,10 @@ package com.enset.cineam_app.dao;
 
 import com.enset.cineam_app.entities.Seance;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@RepositoryRestResource
+@CrossOrigin("*")
 public interface SeanceRepository extends JpaRepository<Seance, Long> {
 }
